963000ffd52df2d43a2e890d432d87c222f89d16
MariCapelo <marina.gac30@gmail.com>
2025-06-03 18:46:02 -0300

    finished GLUD_to_AF function

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
-def GLUD_to_AFN():
-    print("something")
-
+def GLUD_to_AF(G):
+    M = {
+        "Q": [],
+        "alfabeto": [],
+        "transicoes": [],
+        "q0": [],
+        "F": ["Z"],
+        "isAFN": False
+    }
+    
+    transicoes = []
+    
+    for lines in G.keys():
+        if len(M["q0"]) == 0:
+            M["q0"].append(lines)
+            
+        if lines not in M["Q"]:
+            M["Q"].append(lines)
+            
+        for items in G[lines]:
+            if len(items) == 1:
+                if items.islower() or items == "&" or items.isdigit():
+                    transicoes.append([lines, items, "Z"])
+                    if items not in M["alfabeto"]:
+                        M["alfabeto"].append(items) 
+                      
+                elif items.isupper():
+                    M["isAFN"] = True
+                    transicoes.append([lines, "&", items])
+                    if items not in M["Q"]:
+                        M["Q"].append(items)
+            
+            elif len(items) == 2:
+                transicoes.append([lines, items[0], items[1]])
+                if items[0] not in M["alfabeto"]:
+                    M["alfabeto"].append(items[0])
+                M["isAFN"] = True
+    
+    M["transicoes"] = transicoes
+    for keys, values in M.items():
+       print(f"{keys}: {values}")
+    
+# ------------------------------------------------------------------------------------
 
 def ler_arquivo(path):
     with open(path, "r", encoding="utf-8") as file:
@@ -35,7 +75,6 @@ def ler_arquivo(path):
                     
             tam+=1
         
-        print(lines[0])
         GLUD[lines[0]] = P.copy()
         string = ""            
         P.clear()
@@ -45,10 +84,17 @@ def ler_arquivo(path):
     return GLUD
 
 #------------------------------------------------------------------
-# Main Function 
+# Main Function
+print("\nBEM VINDO/A À SIMULAÇÃO DE AUTOMATOS")
 file_path = input("Digite o nome do arquivo:\n")
 gramatic = ler_arquivo(file_path)
-print(gramatic)
 
+print("-"*40)
+print("Você entrou a Gramática Linear a Direita:")
+print(gramatic)
+print("-"*40)
+print("Autômato Finito gerado:")
+GLUD_to_AF(gramatic)
+print("-"*40)
 
     
